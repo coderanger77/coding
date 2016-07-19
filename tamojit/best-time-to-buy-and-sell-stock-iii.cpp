90a1f9f453132c0362ed0769564cc323ce48812f
tamojit chatterjee <tamojit.chatterjee9@gmail.com>
2016-07-20 00:45:49 +0530

    Update best-time-to-buy-and-sell-stock-iii.cpp

--- tamojit/best-time-to-buy-and-sell-stock-iii.cpp
+++ tamojit/best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
 
     int maxProfit(vector<int>& p) {
-        int ans = 0, n = p.size();
-        int f[p.size()+1], b[p.size()+1];
+        int ans = 0, n = p.size(), res = 0;
+        int f[p.size()+1];
         int s = 0, e = 0;
         while(s < n && e < n) {
             ans = max(ans, p[e]-p[s]);
@@ -14,14 +14,11 @@ public:
         s = e = n-1, ans = 0;
         while(s >= 0 && e >= 0) {
             ans = max(ans, p[s]-p[e]);
-            b[e] = ans;
+            res = max(res, f[e-1] + ans);
             if(p[e] <= p[s]) --e;
             else s = e;
         }
-        ans = 0;
-        for(int i = 1; i < n-1; i++) {
-            ans = max(ans, f[i]+b[i+1]);
-        }
-        return max(ans, f[n-1]);
+
+        return max(res, f[n-1]);
     }
 };
