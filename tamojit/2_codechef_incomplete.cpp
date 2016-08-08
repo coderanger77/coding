377db28b07877c3a8d88bcc8a4c6066a1c2fdff1
tamojit chatterjee <tamojit.chatterjee9@gmail.com>
2016-08-08 16:59:03 +0530

    Update 2_codechef_incomplete.cpp

--- tamojit/2_codechef_incomplete.cpp
+++ tamojit/2_codechef_incomplete.cpp
@@ -52,4 +52,58 @@ void inputTestCases() {
 int main() {
     inputTestCases();
 }
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MOD 786433
+#define max3(a, b, c) max(a, max(b, c))
+#define min3(a, b, c) min(a, min(b, c))
+#define vi vector<int> 
+#define pii pair<int, int> 
+#define ll long long 
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define repe(i, n) for(int i = 1; i <= n; i++)
+#define repre(i, n) for(int i = n; i >= 1; i--)
+#define repex(i, n) for(int i = 0; i <= n; i++)
+#define inRange(i, l, r) (i >= l && i <= r)
+
+void preCompute() {
+    
+}
+
+struct result {
+  int n;
+  vi x;
+  result(int n, vi x) : n(n), x(x) {}
+};
+vector<int> graph[600];
+result solve(int m, int x) {
+    
+}
+int handleCase() {
+    int n;
+    cin>>n;
+    repe(i, n) {
+        repe(j, n) graph[i].push_back(j);
+    }
+    int m;
+    cin>>m;
+    while(m--) {
+        int m, x;
+        cin>>m>>x;
+        
+    }
+}
+
+void inputTestCases() {
+    int k = 1;
+    preCompute();
+    while(k--) {
+        cout<<handleCase()<<"\n";
+    }
+}
+int main() {
+    inputTestCases();
+}
+ 
  
